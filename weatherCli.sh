12b7cd92487a9b7474e545e73bbe9a36fa292326
Adam N <aniemcz@clemson.edu>
2023-09-21 22:36:05 -0400

    Added curl to get location of user

--- weatherCli.sh
+++ weatherCli.sh
@@ -11,24 +11,48 @@
 ###
 ###info wanted: temperature
 
-
-echo "hello worldss"
-
 ##curl "https://api.weather.gov/points/39.7456,-97.0892" 
 
 ##curl "https://api.weather.gov/gridpoints/TOP/31,80/forecast"
 
 
-###going to cheat and use python to parse the json
+###This is to get the latitude and longitude of the user based on their ip address
+echo "fetching your ip address and lat and long"
+curl -s "http://ip-api.com/json/" | \
+python3 -c "
+import sys, json;
+from pprint import pprint;
 
+jsonData = json.load(sys.stdin)
+#latitude = jsonData["lat"]
+#longitude = jsonData["long"]
+
+pprint(json.load(sys.stdin))
+"
+
+###Now the lat and long is used to get the office and grid coords which is needed to get the forecast for our region
+
+
+
+###going to cheat and use python to parse the json
+###This if to get the weather forecast data
 curl -s "https://api.weather.gov/gridpoints/TOP/31,80/forecast" | \
 python3 -c "
-import sys, json, pprint;
+import sys, json;
+from pprint import pprint;
+
 jsonOutput =  json.load(sys.stdin)['properties']['periods']
 
 #loop through periods to get the 7 day forecast
+"""
+for x in jsonOutput:
+    pprint(x['name'])
+    pprint(str(x['temperature']) + x['temperatureUnit'])
+    pprint(x['detailedForecast'])
+    print('\n\n')
 
-pprint.pprint(jsonOutput[0])
+"""
+#pprint(jsonOutput[0]['temperature'])
 
 "
 
